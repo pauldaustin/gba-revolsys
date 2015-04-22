5cf2e5d9c697d6853e7ba2c1e667661688425443
Paul Austin <paul.austin@revolsys.com>
2015-04-22 16:18:32 -0700

    esri

--- com.revolsys.open.gis.esri.gdb.file/build-win32.bat
+++ com.revolsys.open.gis.esri.gdb.file/build-win32.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 nmake /f Makefile.win.nmake
 pause
