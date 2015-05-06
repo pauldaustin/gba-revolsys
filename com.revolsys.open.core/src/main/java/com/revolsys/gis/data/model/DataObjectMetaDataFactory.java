758e71da4d152ac94c0b2000f786bb25fcbc435f
Paul Austin <paul.austin@revolsys.com>
2015-05-06 11:17:07 -0700

    rename

--- com.revolsys.open.core/src/main/java/com/revolsys/gis/data/model/DataObjectMetaDataFactory.java
+++
@@ -1,9 +0,0 @@
-package com.revolsys.gis.data.model;
-
-import com.revolsys.data.record.schema.RecordDefinition;
-import com.revolsys.io.ObjectWithProperties;
-
-public interface DataObjectMetaDataFactory extends ObjectWithProperties {
-  RecordDefinition getRecordDefinition(String path);
-
-}
