b7578ce2039b1130ceccd78c9d1d3e862b2b6783
Paul Austin <paul.austin@revolsys.com>
2015-06-08 15:22:14 -0700

    New files

---
+++ com.revolsys.open.core/src/main/java/com/revolsys/collection/set/Sets.java
@@ -0,0 +1,41 @@
+package com.revolsys.collection.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+  public static <V> Set<V> hash(final Iterable<V> values) {
+    final Set<V> set = new HashSet<>();
+    for (final V value : values) {
+      set.add(value);
+    }
+    return set;
+  }
+
+  public static <V> Set<V> hash(@SuppressWarnings("unchecked") final V... values) {
+    final Set<V> set = new HashSet<>();
+    for (final V value : values) {
+      set.add(value);
+    }
+    return set;
+  }
+
+  public static <V> LinkedHashSet<V> linkedHash(final V value) {
+    final LinkedHashSet<V> set = new LinkedHashSet<>();
+    if (value != null) {
+      set.add(value);
+    }
+    return set;
+  }
+
+  public static <V> TreeSet<V> tree(final V value) {
+    final TreeSet<V> set = new TreeSet<>();
+    if (value != null) {
+      set.add(value);
+    }
+    return set;
+  }
+
+}
