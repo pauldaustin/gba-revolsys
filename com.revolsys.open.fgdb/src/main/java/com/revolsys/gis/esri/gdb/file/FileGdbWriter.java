f7827e1311a56e866e402d5f8060d662f8cbff93
Paul Austin <paul.austin@revolsys.com>
2015-07-06 14:47:20 -0700

    x

--- com.revolsys.open.fgdb/src/main/java/com/revolsys/gis/esri/gdb/file/FileGdbWriter.java
+++ com.revolsys.open.fgdb/src/main/java/com/revolsys/gis/esri/gdb/file/FileGdbWriter.java
@@ -60,7 +60,8 @@ public class FileGdbWriter extends AbstractRecordWriter {
       final String typePath = recordDefinition.getPath();
       final Table table = getTable(typePath);
       final String whereClause = "OBJECTID=" + objectId;
-      final EnumRows rows = this.recordStore.search(typePath, table, "OBJECTID", whereClause, false);
+      final EnumRows rows = this.recordStore.search(typePath, table, "OBJECTID", whereClause,
+        false);
       if (rows != null) {
         try {
           final Row row = this.recordStore.nextRow(rows);
@@ -98,7 +99,8 @@ public class FileGdbWriter extends AbstractRecordWriter {
 
   private void insert(final Record record) {
     final RecordDefinition sourceRecordDefinition = record.getRecordDefinition();
-    final RecordDefinition recordDefinition = this.recordStore.getRecordDefinition(sourceRecordDefinition);
+    final RecordDefinition recordDefinition = this.recordStore
+      .getRecordDefinition(sourceRecordDefinition);
 
     validateRequired(record, recordDefinition);
 
@@ -120,12 +122,14 @@ public class FileGdbWriter extends AbstractRecordWriter {
           }
         }
         this.recordStore.insertRow(table, row);
-        for (final FieldDefinition field : recordDefinition.getFields()) {
-          final AbstractFileGdbFieldDefinition esriField = (AbstractFileGdbFieldDefinition)field;
-          try {
-            esriField.setPostInsertValue(record, row);
-          } catch (final Throwable e) {
-            throw new ObjectPropertyException(record, field.getName(), e);
+        if (sourceRecordDefinition == recordDefinition) {
+          for (final FieldDefinition field : recordDefinition.getFields()) {
+            final AbstractFileGdbFieldDefinition esriField = (AbstractFileGdbFieldDefinition)field;
+            try {
+              esriField.setPostInsertValue(record, row);
+            } catch (final Throwable e) {
+              throw new ObjectPropertyException(record, field.getName(), e);
+            }
           }
         }
         record.setState(RecordState.Persisted);
@@ -154,7 +158,8 @@ public class FileGdbWriter extends AbstractRecordWriter {
       insert(record);
     } else {
       final RecordDefinition sourceRecordDefinition = record.getRecordDefinition();
-      final RecordDefinition recordDefinition = this.recordStore.getRecordDefinition(sourceRecordDefinition);
+      final RecordDefinition recordDefinition = this.recordStore
+        .getRecordDefinition(sourceRecordDefinition);
 
       validateRequired(record, recordDefinition);
 
